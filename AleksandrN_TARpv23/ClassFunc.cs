dbd5328bd3a5708e1c7508219a85884cc9a4e8bc
opilane <opilane@HP-CZC2349HTN>
2024-08-26 16:14:38 +0300

    Add project files.

---
+++ AleksandrN_TARpv23/ClassFunc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AleksandrN_TARpv23
+{
+    internal class ClassFunc
+    {
+        
+        public static double Matematika(double arv1, double arv2, char teha)
+        {
+            if (teha == '+')
+            {
+                return arv1 + arv2;
+
+            }
+            else if (teha == '-')
+            {
+                return arv1 - arv2;
+            }
+            else if (teha == '*')
+            {
+                return arv1 * arv2;
+            }
+            else if (teha == '/')
+            { 
+                return arv1 / arv2;
+            }
+            return 0;
+        }
+
+
+        //4
+        public static double StartPrice(double price)
+        {
+            return price / 0.7;
+        
+        }
+
+
+        //8
+        public static 
+    }
+}
---
+++ AleksandrN_TARpv23/ClassMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AleksandrN_TARpv23
+{
+    public class ClassMain
+    {
+        public static void Main(string[] args)
+        {
+            //Console.OutputEncoding = Encoding.UTF8;
+            //Console.WriteLine("Hello everybody!");
+            //string nimetus = "Python";
+            //Console.WriteLine("Bye bye, {0}", nimetus);
+            //ClassFunc.Tere(nimetus);
+            //Console.Write("Sisests esimine arv: ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("Sisests teine arv: ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("Sisests märk: ");
+            //char value = char.Parse(Console.ReadLine());
+            //double vastus = ClassFunc.Matematika(a, b, value);
+            //Console.WriteLine(vastus);
+
+
+            //4
+            Console.Write("Sisests hind: ");
+            double price = double.Parse(Console.ReadLine());
+            double vastus = ClassFunc.StartPrice(price);
+            Console.WriteLine("Algne hind on, {0}", vastus);
+
+            //8
+
+        }
+    }
+}
